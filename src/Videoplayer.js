f8af684d0401fc45793f7b4cd310764548b448cd
Ronny Brzeski <Svenbrzeski3@freenet.de>
2021-11-07 12:34:59 +0100

    Add timeline base

--- src/Videoplayer.js
+++ src/Videoplayer.js
@@ -3,8 +3,9 @@ import "./style/Videoplayer.css";
 import video from "./assets/video.mp4";
 import useVideoPlayer from "./hooks/useVideoPlayer";
 import "boxicons";
+import Timeline from "timeline-editor-react";
 
-const Ui = () => {
+export default () => {
   const videoElement = useRef(null); //create videoElement's reference
 
   //get playerState and functions from hook
@@ -52,8 +53,47 @@ const Ui = () => {
             )}
           </button>
         </div>
+        <div className="timeline">
+        <Timeline layers={layers} frames={frames} onUpdateFrames={onUpdateFrames} />
+      </div>
       </div>
     </div>
   )
 };
-export default Ui;
+
+var layers = [
+  {
+    id: "3d1df1b4-4d9d-45a4-bf14-cb580ee74675",
+    name: "Left"
+  },
+  {
+    id: "7d8c4210-0cfa-4a10-8b21-01e6601e00bf",
+    name: "Top"
+  },
+  {
+    id: "65079f30-47a8-4469-833e-4f0eea04d233",
+    name: "Bottom"
+  }
+];
+var frames = {
+  "3d1df1b4-4d9d-45a4-bf14-cb580ee74675": [{
+    name: "Hello.png",
+    second: 0,
+    duration: 70
+  },
+  {
+    name: "Welcome.png",
+    second: 130,
+    duration: 200
+  }],
+  "7d8c4210-0cfa-4a10-8b21-01e6601e00bf": [{
+    name: "Goodbye.png",
+    second: 10,
+    duration: 150
+  }],
+  "65079f30-47a8-4469-833e-4f0eea04d233": []
+};
+
+function onUpdateFrames(frames) {
+  //TODO: deal with frames
+}
