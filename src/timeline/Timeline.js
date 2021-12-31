a1236a357b66e04c4fcef990b41343c72385073d
Ronny Brzeski <Svenbrzeski3@freenet.de>
2021-12-31 12:42:29 +0100

    Add timeline progress

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -1,54 +1,94 @@
 import React, { Component } from 'react';
 
-export default class Timeline extends Component {
-    render() {
-        const timelineScale = {
-            duration: 360_000,          // gesamt dauer
-            currentProgress: 69         // progress in %
-        }
-
-        const filterBars = [
-            {
-                filterId: 0,
-                filters: [
-                    {
-                        filterId: 0,
-                        startTime: 0,
-                        duration: 10,
-                    }
-                ]
-            },
-            {
-                filterId: 1,
-                filters: [
-                    {
-                        filterId: 1,
-                        startTime: 10,
-                        duration: 20,
-                    },
-                    {
-                        filterId: 1,
-                        startTime: 50,
-                        duration: 70,
-                    }
-                ]
-            },
-        ]
-
-        return (
-            <div className="Timeline" >
-                <h2>Timeline Title</h2>
+export default () => {
+    const filterBars = [
+        {
+            filterId: 0,
+            filters: [
                 {
-                    filterBars.map(filterBar => (
-                        <div className="filterBar">
-                            {filterBar.filters.map(filter => (
-                                <div>
-                                    <b>filterStart= {filter.startTime} | duration= {filter.duration}; </b>
-                                </div>
-                            ))}
-                        </div>
-                    ))}
-            </div>
-        )
-    }
+                    index: 0,
+                    isDragging: false,
+                    filterId: 0,
+                    startTime: 0,
+                    duration: 10,
+                }
+            ]
+        },
+        {
+            filterId: 1,
+            filters: [
+                {
+                    index: 1,
+                    isDragging: false,
+                    filterId: 1,
+                    startTime: 10,
+                    duration: 20,
+                },
+                {
+                    index: 2,
+                    isDragging: false,
+                    filterId: 1,
+                    startTime: 150,
+                    duration: 70,
+                }
+            ]
+        },
+    ];
+
+    return (
+        <div className="Timeline" >
+            <h2>Timeline Title</h2>
+            {
+                filterBars.map(filterBar => (
+                    <div className="filterBar" style={{
+                        border: "1px solid black",
+                        tablelayout: "fixed",
+                        width: "1000px",
+                        position: "relative",
+                        height: "20px"
+                    }}>
+                        {filterBar.filters.map(filter => (
+                            <div className="filterBox" id={filter.index} style={{
+                                position: "absolute",
+                                cursor: "move",
+                                display: "flex",
+                                boxSizing: "border-box",
+                                tableLayout: "fixed",
+                                backgroundColor: 'green',
+                                width: filter.duration * 5 + 'px',
+                                left: filter.startTime,
+                                height: "20px",
+
+                                draggable: 'true',
+                                border: "1px solid black",
+                                overflow: "hidden",
+                                resize: 'horizontal',
+                                whiteSpace: "nowrap"
+                            }}
+
+                                onMouseDown={() => {
+                                    filter.isDragging = true
+                                }}
+
+                                onMouseUp={() => {
+                                    filter.isDragging = false
+                                }}
+                                onMouseMove={(e) => {
+                                    if (filter.isDragging) {
+                                        //console.log("startTime: " + filter.startTime)
+                                        const currentClientX = e.clientX;
+                                        const moveMouseX = currentClientX;
+                                        filter.startTime = moveMouseX;
+                                        document.getElementById(filter.index).style.left = filter.startTime - (filter.duration * 5 / 2) + "px";
+                                        console.log("left: " + document.getElementById(filter.index))
+                                    }
+                                }}
+                            >
+                            </div>
+                        ))}
+                    </div>
+                ))
+            }
+        </div >
+    )
 }
