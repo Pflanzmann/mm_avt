ee2dc61fd558f97c56632750c6cccc19dd0a2d50
Ekaterina <ekaterina94uvao@gmail.com>
2022-01-14 13:22:02 +0100

    upload video automatically

--- src/helper/FileUploader.js
+++ src/helper/FileUploader.js
@@ -1,20 +1,20 @@
-import {useState} from "react";
 import axios from "axios";
 
 export const FileUploader = ({onSuccess, visibility}) => {
-    const [uploadedVideo, setUploadedVideo] = useState()
     const onInputChange = (event) => {
         console.log(event.target.files[0])
-        if (event.target.files[0].type === "video/mp4")
-            setUploadedVideo(event.target.files[0])
+        if (event.target.files[0].type === "video/mp4") {
+            displayVideo(event, event.target.files[0])
+        }
+
     }
-    
-    const handleSubmit = (event) => {
+
+    const displayVideo = (event, video) => {
         event.preventDefault();
 
         const data = new FormData();
 
-        data.append('file', uploadedVideo);
+        data.append('file', video);
         console.log(data)
         axios.post('//localhost:5000/upload', data)
             .then((res) => {
@@ -28,12 +28,11 @@ export const FileUploader = ({onSuccess, visibility}) => {
     }
 
     return (
-        <form method="post" action="#" id="#" onSubmit={handleSubmit} className={visibility}>
+        <form method="post" action="#" id="#" className={visibility}>
             <div className="form-group files">
-                <label>Upload your video</label>
+                <label>Upload your mp4 video</label>
                 <input type="file" onChange={onInputChange} className="form-control"/>
             </div>
-            <button>Upload</button>
         </form>
     )
 }
