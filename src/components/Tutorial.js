dbcd512efe2bf7ced55558495f7bd8dfb5a59c49
Paula <s0573372@htw-berlin.de>
2022-01-22 14:07:03 +0100

    finalize Tutorial styling

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -52,7 +52,7 @@ const Modal = ({ handleClose, show, children }) => {
     <div className={showHideClassName}>
       <section className='modal-main'>
         {children}
-        <button
+        <button className= "tutorialclsbtn"
           onClick={handleClose}
         >
           Close
