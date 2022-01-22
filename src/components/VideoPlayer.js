7ad562c81be195472498783689847e3a099f6d2b
S0571740 <philip.blankenburg@student.htw-berlin.de>
2022-01-22 12:25:47 +0100

    make audio filter work after uploading

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -5,6 +5,25 @@ import { Video } from "../helper/Video";
 import axios from "axios";
 import ApplyFilters from "../helper/ApplyFilters";
 import { useBeforeunload } from 'react-beforeunload';
+import * as ConstantBooleans from "../constants/checkBoxBooleans.js"
+
+
+var videoElement;
+const ctx = new AudioContext();
+const lowpass = ctx.createBiquadFilter();
+const bandpass = ctx.createBiquadFilter();
+const highpass = ctx.createBiquadFilter();
+const lowshelf = ctx.createBiquadFilter();
+const highshelf = ctx.createBiquadFilter();
+const peaking = ctx.createBiquadFilter();
+const notch = ctx.createBiquadFilter();
+lowpass.type = 'lowpass';
+bandpass.type = 'bandpass';
+highpass.type = 'highpass';
+lowshelf.type = 'lowshelf';
+highshelf.type = 'highshelf';
+peaking.type = 'peaking';
+notch.type = 'notch';
 
 export default () => {
     const [uploderVisibility, setUploaderVisibility] = useState("visible")
@@ -32,6 +51,45 @@ export default () => {
         setUploadedVideo(uploadedVideo)
         setUploaderVisibility("hidden")
         setVideoVisibility("visible")
+        setAudioFilter();
+    }
+
+    function setAudioFilter(){
+
+        //This works, but only if the browser was refreshed due to changes in js
+        //TODO: react on checkbox changes without uploading a new video
+        //TODO: make it work after browsertab refresh - does work on firefox...
+
+        videoElement = document.getElementById("videoSource");
+        var mediaElement = ctx.createMediaElementSource(videoElement);
+        mediaElement.connect(lowpass);
+        
+        lowpass.frequency.value = ConstantBooleans.checkBoxBooleans.lowpassChecked ? 4000 : 24000;
+        lowpass.connect(bandpass);
+
+        bandpass.frequency.value = 12000;
+        bandpass.Q.value = ConstantBooleans.checkBoxBooleans.bandpassChecked ? 3000 : 0;
+        bandpass.connect(highpass);
+        
+        highpass.frequency.value = ConstantBooleans.checkBoxBooleans.highpassChecked ? 18000 : 0;
+        highpass.connect(lowshelf);
+        
+        lowshelf.gain.value = 20;
+        lowshelf.frequency.value = ConstantBooleans.checkBoxBooleans.lowshelfChecked ? 6000 : 0;
+        lowshelf.connect(highshelf);
+        
+        highshelf.gain.value = 20;
+        highshelf.frequency.value = ConstantBooleans.checkBoxBooleans.highshelfChecked ? 18000 : 24000;
+        highshelf.connect(peaking);
+        
+        peaking.gain.value = 20;
+        peaking.frequency.value = 12000;
+        peaking.Q.value = ConstantBooleans.checkBoxBooleans.peakingChecked ? 3000 : 0;
+        peaking.connect(notch);
+        
+        notch.frequency.value = 12000;
+        notch.Q.value = ConstantBooleans.checkBoxBooleans.notchChecked ? 3000 : 24000;
+        notch.connect(ctx.destination);
     }
 
     function downloadVideo() {
@@ -74,7 +132,7 @@ export default () => {
             <ApplyFilters handleFilters={applyFilters} disableButton={uploadedVideo == null} />
             <div />
             <button disabled={!filtered} onClick={downloadVideo}>Download</button>
-            <button disabled={uploadedVideo == null} onClick={loadNewVideo}>Upload new video</button>
+            <button onClick={loadNewVideo}>Upload new video</button>
         </div>
     )
 }
