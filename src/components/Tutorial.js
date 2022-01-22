e69cee583562c994d39e443cf8b8323bc5a32945
Paula <s0573372@htw-berlin.de>
2022-01-22 14:35:57 +0100

    add animation to tutorial

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -24,17 +24,9 @@ class Tutorial extends Component {
     return (
       <main>
         <Modal show={this.state.show} handleClose={this.hideModal}>
-          <p>Hier steht Tutorial stuff
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
-            sed diam nonumy eirmod tempor invidunt ut labore et dolore
-             magna aliquyam erat, sed diam voluptua. At vero eos et 
-             accusam et justo duo dolores et ea rebum. Stet clita kasd 
-             gubergren, no sea takimata sanctus est Lorem ipsum dolor 
-             sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing 
-             elitr, sed diam nonumy eirmod tempor invidunt ut labore et 
-             dolore magna aliquyam erat, sed diam voluptua. At vero eos 
-             et accusam et justo duo dolores et ea rebum. Stet clita 
-             kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          <p>
+            <h2>HOW TO PLAY</h2>
+            <p>fdjkghafhajfgjkfvb</p>
           </p>
         </Modal>
         <button className="tutorialbtn" type="button" onClick={this.showModal}>
