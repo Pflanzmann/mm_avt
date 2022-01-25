abd2674fc3e61f920a20ec5926e6760fbba72115
Paula <s0573372@htw-berlin.de>
2022-01-25 13:33:08 +0100

    adjust default filterBars

--- src/Constants/FilterBars.js
+++ src/Constants/FilterBars.js
@@ -5,25 +5,11 @@ export let filterBars = [
             {
                 filterId: 0,
                 startTime: 20,
-                duration: 30,
+                duration: 50,
             }
         ]
     },
-    {
-        filterId: 1,
-        filters: [
-            {
-                filterId: 1,
-                startTime: 0,
-                duration: 40,
-            },
-            {
-                filterId: 1,
-                startTime: 100,
-                duration: 100,
-            }
-        ]
-    },
+
 ]
 
 export const setFilterBars = (newFilterBars) => {
