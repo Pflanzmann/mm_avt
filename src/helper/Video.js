c3c3c2085bbd7ba92f94694f3069fe2a3efa0049
Ekaterina <e.krysenkova@avm.de>
2022-01-27 10:10:23 +0100

    prevent video from playing in the background

--- src/helper/Video.js
+++ src/helper/Video.js
@@ -1,27 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
-export const Video = ({ uploadedVideo, filtered, visibility }) => {
+/**
+ * Represents video component
+ * @param uploadedVideo video that has been uploaded by the user
+ * @param filtered boolean value to choose what video to display: original or filtered
+ * @param visibility boolean value to set visibility of a component
+ * @returns {JSX.Element} video component or empty div component if there is no video to display
+ */
+export const Video = ({uploadedVideo, filtered, visibility}) => {
+
     const [videoSource, setVideoSource] = useState()
-    useEffect(() => {
-        if (uploadedVideo === undefined || uploadedVideo === null) {
-            return <div />
-        }
-        if (filtered) {
-            console.log("filtered")
-            setVideoSource(`//localhost:5000/filtered.mp4`)
-        } else {
-            document.getElementById("video-wrapper").className = "visible"
 
-            console.log(uploadedVideo.filename)
-            setVideoSource(`//localhost:5000/${uploadedVideo.filename}`)
-        }
-    }
+    useEffect(() => {
+            if (uploadedVideo === undefined || uploadedVideo === null) {
+                if (videoSource != null) {
+                    setVideoSource(null)
+                }
+                return <div/>
+            }
+            if (filtered) {
+                setVideoSource(`//localhost:5000/filtered.mp4`)
+            } else {
+                document.getElementById("video-wrapper").className = "visible"
+                setVideoSource(`//localhost:5000/${uploadedVideo.filename}`)
+            }
+        }, [uploadedVideo, filtered, videoSource]
     )
 
     return <div id="video-wrapper" className={visibility}>
         <video id="videoSource" key={videoSource} crossOrigin="anonymous" className="video-frame" controls loop>
-            <source src={videoSource} />
+            <source src={videoSource}/>
         </video>
-
     </div>
 }
