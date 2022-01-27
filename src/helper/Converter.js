eed7b260537f54c936ef5e6bdc711d20be956cb6
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-01-27 16:18:26 +0100

    Adjust video filter ids

--- src/helper/Converter.js
+++ src/helper/Converter.js
@@ -12,7 +12,7 @@ export function convertFilters() {
 
             let startTimePercent = 100 / filterBarRight * startTime;
             let endTimePercent = 100 / filterBarRight * endTime;
-            
+
             let relativeVideoStart = videoDuration / 100 * startTimePercent;
             let relativeVideoEnd = videoDuration / 100 * endTimePercent;
             relativeVideoEnd = Math.min(relativeVideoEnd, videoDuration)
@@ -24,23 +24,23 @@ export function convertFilters() {
 
             switch (filter.filterId) {
                 case 0:
-                    request.push(`unsharp=7:7:-2:7:7:-2:enable=\'between(t,${startTime},${endTime})\'`)
-                    break;
-                case 1:
                     request.push(`hue=s=0:enable=\'between(t,${startTime},${endTime})\'`)
                     break;
+                case 1:
+                    request.push(`unsharp=7:7:-2:7:7:-2:enable=\'between(t,${startTime},${endTime})\'`)
+                    break;
                 case 2:
-                    request.push(`rgbashift=rh=15:bv=15:gh=-15:enable=\'between(t,${startTime},${endTime})\'`)
-                    break;
-                case 3:
-                    request.push(`geq=r='255-r(X,Y)':g='255-g(X,Y)':b='255-b(X,Y)':enable=\'between(t,${startTime},${endTime})\'`)
-                    break;
-                case 4:
                     request.push(`noise=alls=100:allf=t+u:enable=\'between(t,${startTime},${endTime})\'`)
                     break;
-                case 5:
+                case 3:
+                    request.push(`rgbashift=rh=15:bv=15:gh=-15:enable=\'between(t,${startTime},${endTime})\'`)
+                    break;
+                case 4:
                     //can also make cold filter with colortemperature=9000:pl=1
                     request.push(`colortemperature=3000:pl=1:enable=\'between(t,${startTime},${endTime})\'`)
+                    break;
+                case 5:
+                    request.push(`geq=r='255-r(X,Y)':g='255-g(X,Y)':b='255-b(X,Y)':enable=\'between(t,${startTime},${endTime})\'`)
                     break;
                 default:
                     break;
