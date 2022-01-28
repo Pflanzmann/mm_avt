3443af24c7773339010dd16eab824ac3f51997ea
Paula <s0573372@htw-berlin.de>
2022-01-28 19:08:06 +0100

    finalize tutorial text

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -28,22 +28,27 @@ class Tutorial extends Component {
             <h2>Tutorial</h2>
             <p>
               <p>
-                Click the "UPLOAD"-Button to upload a video.<br />
-                If you want to upload a different video, click the "UPLOAD NEW VIDEO"-Button.
+                To get you started, we've included a video for you to try out our cool filters.
               </p>
               <p>
                 Choose a video-filter from the gallery on the left and simply click-and-drag it
                 into the filter-timeline. <br />
                 There you can adjust its size and position. Feel free to add as many filters as you like.<br />
-                You can see the effects of your added filters by clicking the Play-Button in the video player.<br />
+                Once you click "APPLY FILTER", you can see the effects of your added filters by clicking the play button in the video player.<br />
                 If you're happy with the result, click the "DOWNLOAD"-Button to download your new masterpiece. <br />
-                Changed your mind? No problem! Simply double-click the filter you don't like and we'll pretend, it never happened.
+                Changed your mind? No problem! Simply double-click the filter in the timeline you don't like and we'll pretend, it never happened.
               </p>
               <p>
-                If something doesn't sound right, try applying some audio filter by checking the boxes.
+                Ready to try it on your own video material? <br />
+                Click the "UPLOAD"-Button to upload a video.<br />
+                If you want to upload a different video, click the "UPLOAD NEW VIDEO"-Button. <br />
+                And simply repeat the filter-adding process to turn your video into your super cool video.
               </p>
               <p>
-                You can always come back to this tutorial by clicking the ? at the top of the page.
+                If something doesn't sound right, try applying some audio filters by checking the boxes.
+              </p>
+              <p>
+                You can always come back to this tutorial by clicking the "?" at the top of the page.
               </p>
             </p>
           </p>
